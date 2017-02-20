57a92d57f4f1cbff5f27eacf32c21d8e1b71f3be
Luyc <526597516@qq.com>
2017-02-20 14:24:19 +0800

    feat: Add install script

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-#! bin/usr/bash
+#! /bin/zsh
+DATA_MAC_DIR="/Users/$USER/Library/Application\ Support/Sublime\ Text\ 3"
+DATA_LINUX_DIR="/home/$USER/.Sublime Text 3"
+
+if uname -o 
+then 
+    DATA_DIR=$DATA_LINUX_DIR
+else
+    DATA_DIR=$DATA_MAC_DIR
+fi
+
+cp -R html5 "$DATA_DIR/Packages/User/"
